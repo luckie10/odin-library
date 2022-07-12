77f52a760c22a57b7b4747d0ca01294c8f1f0c03
Boban Kilvin <btrajcevski10@gmail.com>
2022-07-12 03:02:05 -0400

    Refactor to module pattern

--- script.js
+++ script.js
@@ -1,48 +1,70 @@
-const myLibrary = []
-
-function Book(title, author, pages, read) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = read;
+const book = (title, author, pages, read)  => {
+	return Object.assign({title, author, pages, read})
 }
 
-function addBookToLibrary() {
-	
-}
+const myLibrary = (function() {
+	const books = [];
 
-const AGoT = new Book(
+	const mainContainer = document.querySelector("main.container");
+	const cardAdd = mainContainer.querySelector('.card-add');
+
+	const AGoT = book(
 		"A Game of Thrones",
 		"George R. R. Martin",
 		834,
 		false
-);
-const leviathanWakes = new Book(
+	);
+
+	const leviathanWakes = book(
 		"Leviathan Wakes",
 		"James S. A. Corey",
 		577,
 		true
-);
-
-myLibrary.push(AGoT);
-myLibrary.push(leviathanWakes);
-
-myLibrary.forEach(book => render(book)); 
-
-function render(book) {
-	const mainContainer = document.querySelector("main.container");
-	const cardAdd = mainContainer.querySelector('.card-add');
-	const card = document.createElement('div');
-
-	card.classList.add('card');
+	);
 	
-	Object.values(book).forEach(value => appendElement(value));
+	books.push(AGoT);
+	books.push(leviathanWakes);
 
-	mainContainer.insertBefore(card, cardAdd);
+	render();
 
-	function appendElement(content) {
-		const element = document.createElement('div');
-		element.textContent = content;
-		card.appendChild(element);
+	function render() {
+		clearCards();
+		books.forEach(libraryBook => createBookCard(libraryBook));
+		createNewBookCard();
 	}
-}
+	
+	function clearCards() {
+		while (mainContainer.firstChild){
+			mainContainer.removeChild(mainContainer.firstChild);
+		}
+	}
+
+	function createBookCard(libBook) {
+		const card = document.createElement('div');
+		card.classList.add('card');
+		
+		Object.values(libBook).forEach(value => appendElement(value));
+		mainContainer.insertBefore(card, cardAdd);
+
+		function appendElement(content) {
+			const element = document.createElement('div');
+			element.textContent = content;
+			card.appendChild(element);
+		}
+
+	}
+	
+	function createNewBookCard() {
+		const card = document.createElement('div');
+		card.classList.add('card');
+		card.classList.add('card-add');
+
+		const button = document.createElement('button');
+		button.textContent = 'New Book';
+		card.appendChild(button);
+
+		mainContainer.appendChild(card);
+	}
+	
+	return {};
+})();
